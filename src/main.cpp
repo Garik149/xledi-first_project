dc9f71b3bebdc6df53aa9a3f415b0ce7343247e7
Gorislav149 <gorislav.goglev@gmail.com>
2021-10-14 12:49:27 +0300

    New

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+
+#include "mainwindow.h"
+
+QPalette darkStyle()
+{
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(46, 52, 57));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(30, 33, 36));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(46, 52, 57));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    return darkPalette;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    //app.setWindowIcon(QIcon{":/images/X-LEdiIcon.png"});
+    //app.setPalette(darkStyle());
+
+    MainWindow window{};
+    window.show();
+
+    return app.exec();
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtWidgets>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void SLnewFile();
+    void SLopenFile();
+    void SLsaveFile();
+    void SLquit();
+
+private:
+    void initActions();
+    void createScene();
+    void createMenu();
+    void createToolBar();
+    void createStatusBar();
+
+    QAction *AnewFile;
+    QAction *AopenFile;
+    QAction *AsaveFile;
+    QAction *Aquit;
+
+    QMenu* fileMenu;
+    QToolBar* toolBar;
+    QStatusBar* statusBar;
+    QTextEdit *textEditor;
+    QGraphicsScene* scene;
+    QGraphicsView* view;
+
+};
+#endif // MAINWINDOW_H
